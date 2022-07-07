b09c7940d145b447031f8638d91cf41d5c70c7f4
manishkumar2005 <104591468+manishkumar2005@users.noreply.github.com>
2022-07-07 05:50:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".Header");
-
-btnNavEl.addEventListener("click", function () {
-    headerEl.classList.toggle("nav-open");
-  });
